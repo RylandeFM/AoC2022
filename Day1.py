382c1951a6a8541da0939431194d32d197b31c5c
RylandeFM <frankmennink@gmail.com>
2022-12-01 06:42:56 +0100

    Initial solution

--- Day1.py
+++ Day1.py
@@ -1,1 +1,15 @@
 with open("Input/Day 1.txt", "r") as f: inputString = f.read().splitlines()
+
+def countCalories():
+    elvesCalories, currentList = [], []
+    for item in inputString:
+        if item == "":
+            elvesCalories.append(sum(currentList))
+            currentList=[]
+        else:
+            currentList.append(int(item))
+    elvesCalories.sort(reverse=True)
+    print(elvesCalories[0])
+    print(sum(elvesCalories[0:3]))
+
+countCalories()
