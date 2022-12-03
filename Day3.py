b445503927e0ab28565a16e0c2f11db6b45ab00b
RylandeFM <frankmennink@gmail.com>
2022-12-03 19:10:29 +0100

    rewritten into one-liners

--- Day3.py
+++ Day3.py
@@ -1,21 +1,13 @@
 with open("Input/Day 3.txt", "r") as f: inputString = f.read().splitlines()
 
 def partOne():
-    sum = 0
-    for line in inputString:
-        dupeList = [x for x in set(line[:len(line)//2]) if x in set(line[len(line)//2:])]
-        sum += getPriority(dupeList[0])
-    print(sum)
+    print(sum([getPriority((set(line[:len(line)//2]) & set(line[len(line)//2:])).pop()) for line in inputString]))
+
+def partTwo():
+    print(sum([getPriority((set(inputString[index+2]) & set(inputString[index+0]) & set(inputString[index+1])).pop()) for index in range(0,len(inputString),3)]))
 
 def getPriority(c):
     return ord(c)-96 if c.islower() else ord(c)-38
-
-def partTwo():
-    sum = 0
-    for index in range(0,len(inputString),3):
-        dupeList = [y for y in set(inputString[index+2]) if y in [x for x in set(inputString[index+0]) if x in set(inputString[index+1])]]
-        sum += getPriority(dupeList[0])
-    print(sum)
 
 partOne()
 partTwo()
