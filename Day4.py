70fd37f675bdcbeb0fb5ca515079170df9778b24
RylandeFM <frankmennink@gmail.com>
2022-12-04 10:14:42 +0100

    One line init

--- Day4.py
+++ Day4.py
@@ -1,8 +1,7 @@
 with open("Input/Day 4.txt", "r") as f: inputString = f.read().splitlines()
 
 def checkContainment():
-    fullyContained = 0
-    partiallyContained = 0
+    fullyContained, partiallyContained = 0, 0
     for line in inputString:
         elf1, elf2 = line.split(",")
         elfSet1, elfSet2 = set(range(int(elf1.split("-")[0]), int(elf1.split("-")[1])+1)), set(range(int(elf2.split("-")[0]), int(elf2.split("-")[1])+1))
