e652b61dbfc1d2c4a91e78a4a6e1ffb9d3dad5e1
RylandeFM <frankmennink@gmail.com>
2022-12-06 09:56:12 +0100

    swapped order to not process first characters unnecessarily

--- Day6.py
+++ Day6.py
@@ -1,8 +1,8 @@
 with open("Input/Day 6.txt", "r") as f: inputString = f.read()
 
 def checkString(amountOfChars):
-    for i in range(len(inputString)-amountOfChars+1):
-        if len(set(inputString[i:i+amountOfChars])) == amountOfChars: return i+amountOfChars
+    for i in range(amountOfChars, len(inputString)):
+        if len(set(inputString[i-amountOfChars:i])) == amountOfChars: return i
 
 print(checkString(4))
 print(checkString(14))
