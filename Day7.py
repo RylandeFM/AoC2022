d9f500d5d31de73d8aae4ee143522ccd907647dd
RylandeFM <frankmennink@gmail.com>
2022-12-07 13:58:33 +0100

    small performance increase

--- Day7.py
+++ Day7.py
@@ -1,28 +1,31 @@
+from functools import cache
+
 with open("Input/Day 7.txt", "r") as f: inputString = f.read().splitlines()
 
 def parseFileSystem():
-    fs, currentDir = {'/': {'parentDir': '', 'subDirs': [], 'files': []}}, '/'
+    fs, currentDir = {'/': {'parentDir': '', 'subDirs': [], 'sizeOfFiles': 0}}, '/'
     for instr in inputString[2:]:
         instr = instr.split(" ")
         if instr[0] == "$" and instr[1] == "cd":
             currentDir = currentDir + "/" + instr[2] if instr[2] != ".." else fs[currentDir]['parentDir']
         elif instr[0] == "dir":
-            fs[currentDir + "/" + instr[1]] = {'parentDir': currentDir, 'subDirs': [], 'files': []}
+            fs[currentDir + "/" + instr[1]] = {'parentDir': currentDir, 'subDirs': [], 'sizeOfFiles': 0}
             fs[currentDir]['subDirs'].append(currentDir + "/" + instr[1])
         elif instr[0] != "$":
-            fs[currentDir]['files'].append(int(instr[0]))
+            fs[currentDir]['sizeOfFiles'] += int(instr[0])
     return fs
 
-def getSize(curDir, fs):
-    totalSize = sum([x for x in fs[curDir]['files']])
+@cache
+def getSize(curDir):
+    totalSize = fs[curDir]['sizeOfFiles']
     for subDir in fs[curDir]['subDirs']:
-        totalSize += getSize(subDir, fs)
+        totalSize += getSize(subDir)
     return totalSize
 
 def prepareForUpdate():
-    fs = parseFileSystem()
-    folderSizes = {x: getSize(x, fs) for x in fs.keys()}
+    folderSizes = {x: getSize(x) for x in fs.keys()}
     print(sum([x for x in folderSizes.values() if x <= 100000]))
     print(min([x for x in folderSizes.values() if x >= (30000000 - (70000000 - folderSizes['/']))]))
 
+fs = parseFileSystem()
 prepareForUpdate()
